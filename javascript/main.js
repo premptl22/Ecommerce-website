abdc67a286149dd314b8f75b5439e52c2fd1c8d0
premptl22 <premptl2204@gmail.com>
2024-11-13 00:47:37 +0530

    search bar updated

--- javascript/main.js
+++ javascript/main.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const categoryCards = document.querySelectorAll(".category-card");
-  categoryCards.forEach((card) => {
-    card.addEventListener("click", () => {
-      alert(`You clicked on ${card.querySelector("h3").innerText}`);
-      // Add navigation or further functionality here
+    const categoryCards = document.querySelectorAll(".category-card");
+    categoryCards.forEach((card) => {
+        card.addEventListener("click", () => {
+        alert(`You clicked on ${card.querySelector("h3").innerText}`);
+        // Add navigation or further functionality here
+        });
     });
-  });
 });
